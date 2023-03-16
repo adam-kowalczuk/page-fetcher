9003f638af11fc0e927206b15f78543b4f69163a
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-16 13:00:10 -0700

    Removed fs.stat function, and console.log printing entirety of text.

--- fetcher.js
+++ fetcher.js
@@ -3,25 +3,15 @@ const fs = require('fs');
 const url = process.argv.slice(2)[0];
 const localFilePath = process.argv.slice(2)[1];
 
-let content = '';
-
 request(url, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for example.edu.
-  content += body;
-    fs.writeFile(localFilePath, content, err => {
-      if (err) {
-        console.error(err);
-      }
-      fs.stat(localFilePath, (err, stats) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        console.log(`Downloaded and saved ${stats.size} bytes to ${localFilePath}`);
-    });
+  console.log('error:', error); 
+  console.log('statusCode:', response && response.statusCode); 
+  fs.writeFile(localFilePath, body, err => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(`Downloaded and saved ${body.length} bytes to ${localFilePath}`)
   });
 });
-
 
