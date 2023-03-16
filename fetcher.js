8d20a18422d7d7b7230e03617da460f5f9cc91ff
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-15 22:23:02 -0700

    Bracket error edit.

--- fetcher.js
+++ fetcher.js
@@ -1,20 +1,27 @@
 const request = require('request');
 const fs = require('fs');
 const url = process.argv.slice(2)[0];
-const localFilePath = process.argv.slice(2)[1]
+const localFilePath = process.argv.slice(2)[1];
 
-const content = '';
+let content = '';
 
 request(url, (error, response, body) => {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+  console.log('body:', body); // Print the HTML for example.edu.
   content += body;
+    fs.writeFile(localFilePath, content, err => {
+      if (err) {
+        console.error(err);
+      }
+      fs.stat(localFilePath, (err, stats) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(`Downloaded and saved ${stats.size} bytes to ${localFilePath}`);
+    });
+  });
 });
 
-fs.writeFile(localFilePath, content, err => {
-  if (err) {
-    console.error(err);
-  }
-  console.log(`Downloaded and saved some bytes to ${localFilePath}`)
-});
+
