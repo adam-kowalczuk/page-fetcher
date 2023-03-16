d052b4a0f1bae0ced466005eaa0e70f818693361
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-15 21:41:22 -0700

    Added code (synchronous at this point) to send a request, and write received information into a local file

--- fetcher.js
+++ fetcher.js
@@ -1,10 +1,20 @@
+const request = require('request');
 const fs = require('fs');
+const url = process.argv.slice(2)[0];
+const localFilePath = process.argv.slice(2)[1]
 
-const content = 'Some content!';
+const content = '';
 
-fs.writeFile('/Users/joe/test.txt', content, err => {
+request(url, (error, response, body) => {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+  content += body;
+});
+
+fs.writeFile(localFilePath, content, err => {
   if (err) {
     console.error(err);
   }
-  // file written successfully
+  console.log(`Downloaded and saved some bytes to ${localFilePath}`)
 });
